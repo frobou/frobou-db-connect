2d230e401f3ec2b028ff637c2d8af13ed8b8fa9b
Fabio Pimenta <fabio@ispti.com.br>
2016-11-21 17:32:58 -0200

    21-11-2016 - work

---
+++ src/Pdo/Db/Interfaces/DbMessagesInterface.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Frobou\Pdo\Db\Interfaces;
+
+interface DbMessagesInterface {
+
+    /**
+     * @param string $more
+     * @return string
+     */
+    public function getGeneric($more);
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getNotInserted();
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getNotDeleted();
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getNotUpdated();
+}
