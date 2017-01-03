5f874ef92abf0eb3f88d840989339c0463c73b90
Fabio Pimenta <fabio@ispti.com.br>
2017-01-03 09:35:13 -0200

    utils

---
+++ src/Pdo/Db/FrobouDbUtils.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Frobou\Pdo\Db;
+
+class FrobouDbUtils
+{
+    public function bindParams($data, $except = [])
+    {
+        $params = [];
+        $fields = '';
+        foreach ($data as $key => $value) {
+            if (in_array($key, $except)) {
+                continue;
+            }
+            $fields .= "{$key}, ";
+            switch (strtoupper(gettype($value))) {
+                case 'BOOLEAN':
+                    array_push($params, ['param' => ":{$key}", 'value' => $value, 'type' => \PDO::PARAM_BOOL]);
+                    break;
+                case 'INTEGER':
+                    array_push($params, ['param' => ":{$key}", 'value' => $value, 'type' => \PDO::PARAM_INT]);
+                    break;
+                case 'NULL':
+                    array_push($params, ['param' => ":{$key}", 'value' => $value, 'type' => \PDO::PARAM_NULL]);
+                    break;
+                default:
+                    array_push($params, ['param' => ":{$key}", 'value' => $value, 'type' => \PDO::PARAM_STR]);
+            }
+        }
+        array_unshift($params, substr($fields, 0, strlen($fields) -2));
+        return $params;
+    }
+}
